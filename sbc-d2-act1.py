97e640c4aa70d143bdd75e53354fab097f47ef6f
junel0321 <123655560+junel0321@users.noreply.github.com>
2024-06-18 21:12:18 -0700

    Add files via upload

---
+++ sbc-d2-act1.py
@@ -0,0 +1,15 @@
+word = "summer bootcamp"
+
+#Summer Bootcamp
+#Summer bootcamP
+#Loot
+#camE
+#AR
+#True = Alpha
+
+print(word.title())
+print(word.capitalize().replace("p","P"))
+print(word[7:11].replace("b", "L"))
+print(word[11:15].replace("p", "E"))
+print(word[12].upper()+word[5].upper())
+print(word.replace(" ", "Wabalo").isalpha())
